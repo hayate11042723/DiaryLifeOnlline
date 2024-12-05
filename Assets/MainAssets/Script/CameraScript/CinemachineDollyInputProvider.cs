847d1cbb98d021af502c46b1f73ea2ab8981ce33
hayate11042723 <135081908+hayate11042723@users.noreply.github.com>
2024-12-05 16:58:06 +0900

    LockOn

---
+++ Assets/MainAssets/Script/CameraScript/CinemachineDollyInputProvider.cs
@@ -0,0 +1,58 @@
+using Cinemachine;
+using UnityEngine;
+
+/// <summary>
+/// CinemachineFramingTransposer のカメラ前後方向のコントロールを付与する
+/// </summary>
+public class CinemachineDollyInputProvider : MonoBehaviour
+{
+    [SerializeField]
+    private CinemachineVirtualCamera _cinemachineVirtualCamera = null;
+
+    [SerializeField]
+    private CinemachineInputProvider _cinemachineInputProvider = null;
+
+    [SerializeField]
+    private float _sensitivity;
+
+    [SerializeField]
+    private float _minDistance;
+
+    [SerializeField]
+    private float _maxDistance;
+
+    public float Sensitivity
+    {
+        get => _sensitivity;
+        set => _sensitivity = value;
+    }
+
+    private void OnEnable()
+    {
+        _cinemachineInputProvider.ZAxis.action.Enable();
+        Debug.Log("Ebable");
+    }
+
+    //private void OnDisable()
+    //{
+    //    _cinemachineInputProvider.ZAxis.action.Disable();
+    //    Debug.Log("Disable");
+    //}
+
+    private void Awake()
+    {
+        var cinemachineComponent = _cinemachineVirtualCamera.GetCinemachineComponent(CinemachineCore.Stage.Body);
+        if (cinemachineComponent is CinemachineFramingTransposer cinemachineFramingTransposer)
+        {
+            _cinemachineInputProvider.ZAxis.action.performed += context =>
+            {
+                float distance = Mathf.Clamp(cinemachineFramingTransposer.m_CameraDistance + context.ReadValue<float>() * _sensitivity, _minDistance, _maxDistance);
+                cinemachineFramingTransposer.m_CameraDistance = distance;
+            };
+        }
+        else
+        {
+            Debug.LogWarning($"{nameof(CinemachineFramingTransposer)} が存在しません。");
+        }
+    }
+}
---
+++ Assets/MainAssets/Script/CameraScript/FocusOnEnemy.cs
@@ -0,0 +1,148 @@
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FocusOnEnemy : MonoBehaviour
+{
+    [SerializeField]
+    private CinemachineVirtualCamera _cinemachineVirtualCamera;
+
+    [SerializeField]
+    private Transform _playerTransform;
+
+    [SerializeField]
+    private InputActionReference _clickAction; // 左クリックアクション
+
+    [SerializeField]
+    private InputActionReference _rightClickDragAction; // 右ドラッグのInputAction
+
+    [SerializeField]
+    private CinemachineInputProvider _inputProvider; // CinemachineのInputProvider
+
+    private Transform _currentLookTarget; // 現在のターゲット
+    private bool _isFocusedOnEnemy = false; // カメラがEnemyを注視しているか
+    private bool _isDragging = false; // 右ドラッグ中かどうか
+
+    private void OnEnable()
+    {
+        // アクションを有効化
+        _clickAction.action.Enable();
+        _clickAction.action.performed += OnLeftClickPerformed;
+
+        _rightClickDragAction.action.Enable();
+        _rightClickDragAction.action.started += OnRightClickDragStart;
+        _rightClickDragAction.action.canceled += OnRightClickDragEnd;
+
+        if (_inputProvider != null)
+        {
+            _inputProvider.enabled = true;
+        }
+    }
+
+    private void OnDisable()
+    {
+        // アクションを無効化
+        _clickAction.action.performed -= OnLeftClickPerformed;
+        _clickAction.action.Disable();
+
+        _rightClickDragAction.action.started -= OnRightClickDragStart;
+        _rightClickDragAction.action.canceled -= OnRightClickDragEnd;
+        _rightClickDragAction.action.Disable();
+
+        if (_inputProvider != null)
+        {
+            _inputProvider.enabled = false;
+        }
+    }
+
+    private void OnLeftClickPerformed(InputAction.CallbackContext context)
+    {
+        if (context.control.name == "leftButton")
+        {
+            if (_isFocusedOnEnemy)
+            {
+                ResetCamera();
+            }
+            else
+            {
+                TryFocusOnClickedEnemy();
+            }
+        }
+    }
+
+    private void OnRightClickDragStart(InputAction.CallbackContext context)
+    {
+        if (context.control.name == "rightButton")
+        {
+            _isDragging = true;
+            if (_inputProvider != null)
+            {
+                _inputProvider.enabled = true; // CinemachineInputProviderを有効化
+            }
+        }
+    }
+
+    private void OnRightClickDragEnd(InputAction.CallbackContext context)
+    {
+        if (context.control.name == "rightButton")
+        {
+            _isDragging = false;
+            if (!_isFocusedOnEnemy && _inputProvider != null)
+            {
+                _inputProvider.enabled = true; // ターゲットがない場合は引き続き有効
+            }
+        }
+    }
+
+    private void TryFocusOnClickedEnemy()
+    {
+        if (_isDragging) return; // 右ドラッグ中はクリック無効
+
+        // Raycastでクリックしたオブジェクトを検出
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            // Enemyタグが付いているオブジェクトを判定
+            if (hit.transform.CompareTag("Enemy"))
+            {
+                FocusCameraOnEnemy(hit.transform);
+            }
+        }
+    }
+
+    private void FocusCameraOnEnemy(Transform enemyTransform)
+    {
+        if (_cinemachineVirtualCamera != null && enemyTransform != null)
+        {
+            // LookAtをクリックしたEnemyに設定
+            _cinemachineVirtualCamera.LookAt = enemyTransform;
+            _currentLookTarget = enemyTransform;
+
+            // CinemachineInputProviderを無効化（Enemyにフォーカス中は操作を禁止）
+            if (_inputProvider != null)
+            {
+                _inputProvider.enabled = false;
+            }
+
+            _isFocusedOnEnemy = true;
+        }
+    }
+
+    private void ResetCamera()
+    {
+        // カメラをプレイヤーにリセット
+        if (_cinemachineVirtualCamera != null)
+        {
+            _cinemachineVirtualCamera.LookAt = _playerTransform;
+        }
+
+        _currentLookTarget = null;
+        _isFocusedOnEnemy = false;
+
+        // CinemachineInputProviderを有効化
+        if (_inputProvider != null)
+        {
+            _inputProvider.enabled = true;
+        }
+    }
+}
